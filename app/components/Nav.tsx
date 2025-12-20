daf68833ae686fb889e08d73b9d38703df110f14
kiaczb <b3n3kin@gmail.com>
2025-12-20 14:52:20 +0100

    Added dark theme logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 const Nav = () => {
+  const { theme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    setIsDarkMode(resolvedTheme === "dark");
+  }, [resolvedTheme]);
   return (
     <div className="sticky top-0 flex justify-between pl-3 pr-8 shadow-md mb-15 w-auto items-center bg-white dark:bg-gray-800 z-1">
       <Image
-        src="/speedcubing.png"
+        src={isDarkMode ? "/speedcubing_dark.png" : "/speedcubing.png"}
         alt="Speedcubing Logo"
         width={200}
         height={100}
